adda879d53c70bfe535aaf71b6bb4064cc2cead2
0x000alek <aolguin.or@gmail.com>
2025-06-20 23:02:19 -0400

    feat: agregar variables de entorno para paginación y ordenamiento

--- config/joyas.config.js
+++ config/joyas.config.js
@@ -13,5 +13,21 @@ export const config = {
     db_password: process.env.DB_PASSWORD || '',
     db_port: process.env.DB_PORT || 5432,
     db_database: process.env.DB_DATABASE || 'mydb',
+
+    queries: {
+      inventario: {
+        allowedOrderBy: [
+          'id',
+          'nombre',
+          'categoria',
+          'metal',
+          'precio',
+          'stock',
+        ],
+        defaultLimits: process.env.INVENTARIO_QUERY_DEFAULT_LIMIT || 10,
+        defaultOrderBy: 'id',
+        defaultPage: process.env.INVENTARIO_QUERY_DEFAULT_PAGE || 1,
+      },
+    },
   },
 };

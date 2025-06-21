d3a804ea7d81754550d18584f3eb044e2c3dcb87
0x000alek <aolguin.or@gmail.com>
2025-06-21 02:07:20 -0400

    feat: añadir endpoint GET /joyas/joya/:id para obtener detalle de una joya

--- routes/joyas.routes.js
+++ routes/joyas.routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
-import { getAllJoyasController } from '../src/controllers/joyas.controller.js';
+import {
+  getAllJoyasController,
+  getJoyaByIdController,
+} from '../src/controllers/joyas.controller.js';
 
 const router = Router();
 
 router.get('/joyas', getAllJoyasController);
+router.get('/joyas/joya/:id', getJoyaByIdController);
 
 export default router;

88ff63139fb679cddd2ccf6ad82d13b5f1719aef
0x000alek <aolguin.or@gmail.com>
2025-06-21 23:45:11 -0400

    refactor: renombrar variable joyasMiddleware a joyasMiddlewares

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import express from 'express';
 import { config } from './config/joyas.config.js';
 
 import logger from './src/helpers/logger.js';
-import joyasMiddleware from './middlewares/joyas.middlewares.js';
+import joyasMiddlewares from './middlewares/joyas.middlewares.js';
 
 import joyasRoutes from './routes/joyas.routes.js';
 
@@ -16,11 +16,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(joyasMiddleware.log());
+app.use(joyasMiddlewares.log());
 
 app.use(joyasRoutes);
 
-app.use(joyasMiddleware.errorHandler());
+app.use(joyasMiddlewares.errorHandler());
 
 app.listen(port, () => {
   logger.info(`Server on fire 🔥 ${serverUrl}`);

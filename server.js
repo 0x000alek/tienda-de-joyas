488aff521889dff26ff99d05d9c5521527f773e0
0x000alek <aolguin.or@gmail.com>
2025-06-21 23:04:30 -0400

    chore: agregar middleware de logging y manejo de errores (log y errorHandler) en app.use

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import express from 'express';
 import { config } from './config/joyas.config.js';
 
 import logger from './src/helpers/logger.js';
+import joyasMiddleware from './middlewares/joyas.middlewares.js';
+
 import joyasRoutes from './routes/joyas.routes.js';
 
 const { protocol, host, port } = config.server;
@@ -14,7 +16,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(joyasMiddleware.log());
+
 app.use(joyasRoutes);
+
+app.use(joyasMiddleware.errorHandler());
 
 app.listen(port, () => {
   logger.info(`Server on fire 🔥 ${serverUrl}`);
